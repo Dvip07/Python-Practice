658a8b818da78b4ba2d6a06698e57098f599401b
Dvip <64122205+Dvip07@users.noreply.github.com>
2022-03-18 00:22:14 +0530

    Add files via upload

---
+++ 3/3.1.py
@@ -0,0 +1,27 @@
+# Numbers, Dates, and Times
+# Rounding Numerical Values
+# 3.1.1
+round(1.23, 1)
+round(1.27, 1)
+round(-1.27, 1)
+round(1.2536, 3)
+
+# 3.1.2
+a = 1627731
+round(a, -1)
+round(a, -2)
+round(a, -3)
+
+# Discussion
+x = 1.23456
+format(x, '0.2f')
+format(x, '0.3f')
+'value is {:0.3f}'.format(x)
+
+# --
+a = 2.1
+b = 4.2
+c = a + b
+c
+c = round(c, 2)
+c
